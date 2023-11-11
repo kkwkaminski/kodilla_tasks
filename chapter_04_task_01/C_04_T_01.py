bc6fc76752b019aa11978fe1349db8d142a08f3f
Kamil Laptop <kkw.kaminski@gmail.com>
2023-11-11 16:42:53 +0100

    add function

--- chapter_04_task_01/C_04_T_01.py
+++ chapter_04_task_01/C_04_T_01.py
@@ -1,1 +1,6 @@
-# test
+def ispalindrome(tekst):
+    # Usuwamy białe znaki i zmieniamy tekst na małe litery
+    tekst = tekst.replace(" ", "").lower()
+    # Porównujemy tekst z odwróconym tekstem
+    return tekst == tekst[::-1]
+

a2cfd8b9ecef8bc258be37ce377f3c435a2c3fe5
Kamil Laptop <kkw.kaminski@gmail.com>
2023-11-11 17:14:11 +0100

    add info function

--- chapter_04_task_01/C_04_T_01.py
+++ chapter_04_task_01/C_04_T_01.py
@@ -1,4 +1,7 @@
 def ispalindrome(tekst):
+    """    
+    Funkcja sprawdzająca palidromiczność tekstu
+    """
     # Usuwamy białe znaki i zmieniamy tekst na małe litery
     tekst = tekst.replace(" ", "").lower()
     # Porównujemy tekst z odwróconym tekstem
@@ -6,3 +9,4 @@ def ispalindrome(tekst):
 
 print(ispalindrome("potop"))  # True
 print(ispalindrome("Python"))  # False
+help(ispalindrome)

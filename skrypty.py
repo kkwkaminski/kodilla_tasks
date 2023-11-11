c84f28c840c43518e47a678918bfa7a51b201b25
Kamil Laptop <kkw.kaminski@gmail.com>
2023-11-11 18:01:38 +0100

    add skrypty

--- skrypty.py
+++ skrypty.py
@@ -1,4 +1,14 @@
-def greeting_from_file():
-    print("Greetings from file!")
+def shopping(items, payment='card', shop='local shop'):
+    result = ""
+    result = result + "Idę na zakupy do %s.\n" % shop
+    result = result + "Kupię następujące rzeczy:\n"
+    for item in items:
+        result = result + " - %s\n" % item
+    result = result + "By zapłacić, używam %s." % payment
+    return result
 
-greeting_from_file()
+if __name__ == "__main__":
+    items_text = input("Podaj proszę produkty rozdzielone przecinkiem: ")
+    items = items_text.split(',')
+    shopping_result = shopping(items)
+    print(shopping_result)

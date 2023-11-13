96c6ed4e0897797b16025920e9f9826e8f11780c
Kamil <kkw.kaminski@gmail.com>
2023-11-13 15:16:50 +0100

    nowe zadania

--- skrypty.py
+++ skrypty.py
@@ -1,14 +1,22 @@
-def shopping(items, payment='card', shop='local shop'):
-    result = ""
-    result = result + "Idę na zakupy do %s.\n" % shop
-    result = result + "Kupię następujące rzeczy:\n"
-    for item in items:
-        result = result + " - %s\n" % item
-    result = result + "By zapłacić, używam %s." % payment
-    return result
+# def shopping(items, payment='card', shop='local shop'):
+#     result = ""
+#     result = result + "Idę na zakupy do %s.\n" % shop
+#     result = result + "Kupię następujące rzeczy:\n"
+#     for item in items:
+#         result = result + " - %s\n" % item
+#     result = result + "By zapłacić, używam %s." % payment
+#     return result
 
-if __name__ == "__main__":
-    items_text = input("Podaj proszę produkty rozdzielone przecinkiem: ")
-    items = items_text.split(',')
-    shopping_result = shopping(items)
-    print(shopping_result)
+# if __name__ == "__main__":
+#     items_text = input("Podaj proszę produkty rozdzielone przecinkiem: ")
+#     items = items_text.split(',')
+#     shopping_result = shopping(items)
+#     print(shopping_result)
+
+with open("names.txt", 'r') as read_file:
+    for line in read_file.read().splitlines():
+        print(line)
+
+new_name = "Luke"
+with open("names.txt", 'a') as write_file:
+    write_file.write(new_name)
